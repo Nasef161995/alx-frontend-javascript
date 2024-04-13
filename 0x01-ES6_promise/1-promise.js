b208396abc915b625023d8485a584333a8ec1aba
Nasef161995 <mnasef905@gmail.com>
2024-04-13 22:34:56 +0300

    edit

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return promise;
+}
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function handleResponseFromAPI(promise) {
+  try {
+    return promise.then(() => ({ status: 200, body: 'success' }))
+      .catch(() => Error())
+      .finally(() => console.log('Got a response from the API'));
+  } catch (error) {
+    return Error();
+  }
+}
