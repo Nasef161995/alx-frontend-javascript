2c825303c1701263598df74fc847420c27009b2b
Nasef161995 <mnasef905@gmail.com>
2024-04-19 19:22:27 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+function cleanSet(set, startString) {
+  const arr = [];
+  if (!(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  [...set].map((element) => {
+    if (element && element.startsWith(startString)) {
+      if (element.slice(startString.length) === '' || startString.length === 0) {
+        // do nothing
+      } else {
+        arr.push(element.slice(startString.length));
+      }
+    }
+    return element;
+  });
+  return arr.join('-');
+}
+export default cleanSet;
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
+export default groceriesList;
